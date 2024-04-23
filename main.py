4e588ad9f10b75a2979817a59f757c2d39e4e77d
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-24 02:25:08 +0700

    Creating Lists Through Literals

--- main.py
+++ main.py
@@ -17,3 +17,46 @@ if __name__ == "__main__":
   print(colors[3])
 
   print([42, "apple", True, { "name": "John Doe" }, (1, 2, 3), [3.14, 2.78]])
+
+  # Constructing Lists in Python
+
+  # Creating Lists Through Literals
+
+  """
+  [item_0, item_1, ..., item_n]
+  """
+
+  digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  print(digits)
+
+  fruits = ["apple", "banana", "orange", "kiwi", "grape"]
+  print(fruits)
+
+  cities = [
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Philadelphia"
+  ]
+  print(cities)
+
+  matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]
+  print(matrix)
+
+  inventory = [
+    { "product": "phone", "price": 1000, "quantity": 10 },
+    { "product": "laptop", "price": 1500, "quantity": 5 },
+    { "product": "tablet", "price": 500, "quantity": 20 }
+  ]
+  print(inventory)
+
+  functions = [print, len, range, type, enumerate]
+  print(functions)
+
+  empty = []
+  print(empty)
